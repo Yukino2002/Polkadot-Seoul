f452c423a3badb087cd3952e65e5d350e469f45e
Pratik Jallan <pratikjalan11@gmail.com>
2023-07-17 01:05:21 +0530

    add rpc endpoint for contract parachain

--- substrate.py
+++ substrate.py
@@ -7,13 +7,17 @@ import requests
 load_dotenv()
 
 base_url = "https://rococo.api.subscan.io"
-substrate = SubstrateInterface(url="wss://rococo-rpc.polkadot.io")
+substrate_relay = SubstrateInterface(url="wss://rococo-rpc.polkadot.io")
+substrate_contract = SubstrateInterface(
+    url='wss://rococo-contracts-rpc.polkadot.io')
 api_key = os.getenv("API_KEY")
 
 
 def format_balance(amount: int):
-    amount = format(amount / 10**substrate.properties.get('tokenDecimals', 0), ".15g")
-    return f"{amount} {substrate.properties.get('tokenSymbol', 'UNIT')}"
+    amount = format(
+        amount / 10**substrate_relay.properties.get('tokenDecimals', 0),
+        ".15g")
+    return f"{amount} {substrate_relay.properties.get('tokenSymbol', 'UNIT')}"
 
 
 def get_account_transfers(account_address):
@@ -26,7 +30,9 @@ def get_account_transfers(account_address):
 
 
 def get_account_balance(account_address):
-    result = substrate.query("System", "Account", ["5DvyRvNq5jpvjat2qkGhiKjJQdz5cwreJW5yxvLBLRpHnoGo"])
+    result = substrate_relay.query(
+        "System", "Account",
+        ["5DvyRvNq5jpvjat2qkGhiKjJQdz5cwreJW5yxvLBLRpHnoGo"])
     balance = (result.value["data"]["free"] + result.value["data"]["reserved"])
 
     return format_balance(balance)
@@ -42,27 +48,24 @@ def get_transfer_details(extrinsic_index):
 
 
 def send_balance(recipient_address, amount):
-    call = substrate.compose_call(
-        call_module='Balances',
-        call_function='transfer',
-        call_params={
-            'dest': recipient_address,
-            'value': amount * 10**15
-        }
-    )
+    call = substrate_relay.compose_call(call_module='Balances',
+                                        call_function='transfer',
+                                        call_params={
+                                            'dest': recipient_address,
+                                            'value': amount * 10**15
+                                        })
 
-    extrinsic = substrate.create_signed_extrinsic(
-        call=call, 
+    extrinsic = substrate_relay.create_signed_extrinsic(
+        call=call,
         keypair=Keypair.create_from_mnemonic(os.getenv("MNEMONIC")),
-        era={'period': 64}
-    )
+        era={'period': 64})
 
     try:
-        receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
-        
+        receipt = substrate_relay.submit_extrinsic(extrinsic,
+                                                   wait_for_inclusion=True)
+
         print('Extrinsic "{}" included in block "{}"'.format(
-            receipt.extrinsic_hash, receipt.block_hash
-        ))
+            receipt.extrinsic_hash, receipt.block_hash))
 
         print(receipt)
 
@@ -72,15 +75,15 @@ def send_balance(recipient_address, amount):
                 print(f'* {event.value}')
         else:
             print('⚠️ Extrinsic Failed: ', receipt.error_message)
-        
+
         return receipt
 
-    except SubstrateRequestException as e:
+    except Substrate_relayRequestException as e:
         print(e)
         return False
 
 
-# print(get_account_balance("5DvyRvNq5jpvjat2qkGhiKjJQdz5cwreJW5yxvLBLRpHnoGo"))
+print(get_account_balance("5DvyRvNq5jpvjat2qkGhiKjJQdz5cwreJW5yxvLBLRpHnoGo"))
 # print(get_account_transfers("5DvyRvNq5jpvjat2qkGhiKjJQdz5cwreJW5yxvLBLRpHnoGo"))
 # print(get_transfer_details("6245445-2"))
-send_balance("5Fe4G8vypjGHPkwwBSF1nbnyX6ZKTMMNVQDhaZJ1u6tafcpF", 0.01)
+# send_balance("5Fe4G8vypjGHPkwwBSF1nbnyX6ZKTMMNVQDhaZJ1u6tafcpF", 0.01)
