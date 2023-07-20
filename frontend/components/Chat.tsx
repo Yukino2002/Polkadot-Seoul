5af9b83242ad46d40f7f628128c84fa0c2480e57
emperorrp <rahulpujari2919@gmail.com>
2023-07-21 02:32:45 +0530

    yes/no option

--- frontend/components/Chat.tsx
+++ frontend/components/Chat.tsx
@@ -1,12 +1,8 @@
-'use client'
-
-import { db } from "@/firebase";
-import { collection, query, getDocs } from "firebase/firestore";
-import { useSession } from "next-auth/react";
-import { useCollection } from "react-firebase-hooks/firestore";
-import Message from "./Message";
-import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
+import { db } from "@/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import Message from "./Message";
 
 type Props = {
   chatId: string,
@@ -16,8 +12,7 @@ type Props = {
 
 const Chat = ({ chatId, reload, setReload }: Props) => {
   const { data: session } = useSession();
-  const [messagesData, setMessagesData] = useState<any[]>([])
-  // let messagesData: any = []
+  const [messagesData, setMessagesData] = useState<any[]>([]);
   const messagesPath = `users/${session?.user?.email}/chats/${chatId}/messages`;
 
   useEffect(() => {
@@ -39,18 +34,16 @@ const Chat = ({ chatId, reload, setReload }: Props) => {
 
     fetchMessages();
   }, [messagesPath, reload])
+
   return (
     <div className="flex-1 overflow-y-auto overflow-x-hidden">
       {messagesData.length === 0 && (
-        <>
-          <ArrowUpCircleIcon className="h-10 w-10 mx-auto mt-5 text-white animate-bounce" />
-          <p className="mt-3 text-center text-white">
-            Type a prompt to get started
-          </p>
-        </>
+        <p className="mt-3 text-center text-white">
+          Type a prompt to get started
+        </p>
       )}
-      {messagesData.map((message: any) => (
-        <Message key={message.id} message={message.message} />
+      {messagesData.map((message: any, index: number) => (
+        <Message key={message.id} message={message.message} isBotMessage={index % 2 !== 0} />
       ))}
     </div>
   )
--- frontend/components/Message.tsx
+++ frontend/components/Message.tsx
@@ -1,22 +1,34 @@
 import { DocumentData } from "firebase/firestore";
 
 type Props = {
-  message: DocumentData
+  message: DocumentData,
+  isBotMessage: boolean
 }
 
 const Message = ({
-  message
+  message,
+  isBotMessage
 }: Props) => {
   console.log(message)
-  const isChatGPT = message.user.name === "ChatGPT";
+
+  const handleYesNoClick = (choice: string) => {
+    console.log(`User selected: ${choice}`);
+  }
+
   return (
-    <div className={`py-5 text-white ${isChatGPT && "bg-gray-500/50"}`}>
+    <div className="py-5 text-white">
       <div className="flex space-x-5 px-10 max-2-2xl mx-auto">
         <img src={message.user.avatar} alt="" className="h-8 w-8" />
         <p className="pt-1 text-sm">
           {message.text}
         </p>
       </div>
+      {isBotMessage && (
+        <div className="flex justify-center mt-2">
+          <button onClick={() => handleYesNoClick('yes')} className="mx-2 py-1 px-3 bg-green-500 text-white rounded-md">Yes</button>
+          <button onClick={() => handleYesNoClick('no')} className="mx-2 py-1 px-3 bg-red-500 text-white rounded-md">No</button>
+        </div>
+      )}
     </div>
   )
 }
