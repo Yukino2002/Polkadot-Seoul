73186f7a75f2403458294702b7792e39c26b4908
emperorrp <rahulpujari2919@gmail.com>
2023-07-21 21:20:09 +0530

    ui changes

--- frontend/components/Message.tsx
+++ frontend/components/Message.tsx
@@ -2,28 +2,36 @@ import { DocumentData } from "firebase/firestore";
 
 type Props = {
   message: DocumentData,
-  isBotMessage: boolean
+  isBotMessage: boolean,
+  isFlag: boolean,
 }
+
+const botAvatar = "https://i.imgur.com/usI3OTw.png"; 
 
 const Message = ({
   message,
-  isBotMessage
+  isBotMessage,
+  isFlag,
 }: Props) => {
   console.log(message)
 
   const handleYesNoClick = (choice: string) => {
     console.log(`User selected: ${choice}`);
+    if (isFlag) {
+      console.log("Executing flag-specific code...");
+      // Add your flag-specific code here
+    }
   }
 
   return (
-    <div className="py-5 text-white">
+    <div className={`py-5 text-white ${isBotMessage ? 'bg-gray-300/10' : ''}`}> {/* add gray background if it's a bot's message */}
       <div className="flex space-x-5 px-10 max-2-2xl mx-auto">
-        <img src={message.user.avatar} alt="" className="h-8 w-8" />
+        <img src={isBotMessage ? botAvatar : message.user.avatar} alt="" className="h-8 w-8" /> {/* use botAvatar if it's a bot's message */}
         <p className="pt-1 text-sm">
           {message.text}
         </p>
       </div>
-      {isBotMessage && (
+      {isBotMessage && isFlag && (
         <div className="flex justify-center mt-2">
           <button onClick={() => handleYesNoClick('yes')} className="mx-2 py-1 px-3 bg-green-500 text-white rounded-md">Yes</button>
           <button onClick={() => handleYesNoClick('no')} className="mx-2 py-1 px-3 bg-red-500 text-white rounded-md">No</button>
--- frontend/components/RightSidebar.tsx
+++ frontend/components/RightSidebar.tsx
@@ -1,14 +1,65 @@
 'use client'
-import { KeyIcon } from "@heroicons/react/24/solid";
+import { FunctionComponent, useState } from 'react';
+import { ClipboardIcon, KeyIcon } from "@heroicons/react/24/solid";
 import { SidebarButton } from "./SidebarButton"
 
+interface CardProps {
+  title: string;
+  children: React.ReactNode;
+}
 
-const RightSidebar = () => {
+const Card: FunctionComponent<CardProps> = ({title, children}) => {
   return (
-    <div className='text-white  items-center p-2 flex flex-col h-screen'>
+    <div className="rounded-lg bg-gray-700 p-4 mb-4">
+      <h3 className="text-xl mb-2">{title}</h3>
+      {children}
+    </div>
+  )
+}
+
+interface TransactionCardProps {
+  amount: string;
+  hash: string;
+}
+
+const TransactionCard: FunctionComponent<TransactionCardProps> = ({amount, hash}) => {
+  const url = `https://polkascan.io/polkadot/transaction/${hash}`;
+  const displayedHash = `${hash.slice(0,6)}...${hash.slice(-4)}`;
+  return (
+    <Card title="Last Transaction">
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        <p>Amount: {amount}</p>
+        <p>Hash: {displayedHash}</p>
+      </a>
+    </Card>
+  )
+}
+
+const RightSidebar: FunctionComponent = () => {
+  const walletBalance = "1000 DOT";
+  const userAddress = "16ZL8yLyXv3V3L3z9ofR1ovFLziyXaN1DPq4yffMAZ9czzBD";
+  const displayedUserAddress = `${userAddress.slice(0,6)}...${userAddress.slice(-4)}`;
+  const userAddressUrl = `https://polkascan.io/polkadot/account/${userAddress}`;
+
+  const lastTransactionAmount = "50 DOT";
+  const lastTransactionHash = "0xaa6a6b06c0a38b5239d13a2e700924ed1bbbaf44d5bbfdbb0a8adcef4d453f71";
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(userAddress);
+  }
+
+  return (
+    <div className='text-white items-center p-2 flex flex-col h-screen'>
         <div className='flex-1'>
             <p className='text-xl py-3'>Polkadot Information</p>
+            <a href={userAddressUrl} target="_blank" rel="noopener noreferrer" className='flex'>
+              <p className='text-l py-1 bg-gray-300/20 px-2 rounded '>Account Address: {displayedUserAddress} </p>  
+            </a>
+            <p className='text-l py-1 bg-gray-300/20 px-2 rounded'>Wallet Balance: {walletBalance}</p>
+            <br />
+            <TransactionCard amount={lastTransactionAmount} hash={lastTransactionHash} />
         </div>
+
 
         <div className="flex justify-center items-center py-2 border-t border-gray-500">
         {/* Mnemonic Key Input */}
